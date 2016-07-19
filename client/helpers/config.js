9662abfcd899559eedba184e09ae43580183caff
inpyokim <inpyokim@gmail.com>
2016-07-19 18:29:18 +0900

    test

---
+++ client/helpers/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by inpyokim on 2016. 7. 19..
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
---
+++ client/templates/application/layout.js
@@ -0,0 +1,6 @@
+/**
+ * Created by inpyokim on 2016. 7. 19..
+ */
+Template.layout.helpers({
+    pageTitle: function() { return Session.get('pageTitle'); }
+});
